8134ddff88883a7ccdee59d38028bfbd4b377bab
Isaac <isaacsouzawrk@gmail.com>
2020-11-22 00:14:57 -0300

    desenvolvido durante o bootcamp avanade fullstack 2020

---
+++ Projeto_APIWEB/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_APIWEB.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório!")]
+
+        public string Descricao { get; set;  }
+
+        [Range(1, 100, ErrorMessage = "Valor fora da Faixa, insira um valor entre 1 e 100")]
+        public int Quantidade { get; set; }
+       
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+
+}
